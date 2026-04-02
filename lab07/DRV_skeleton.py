fecd6b75dd3ee82414fc6c781ee8e6f4178033e7
Ethan Sie <144497224+ethannsie@users.noreply.github.com>
2026-04-02 09:44:45 -0700

    also fixed

--- lab07/DRV_skeleton.py
+++ lab07/DRV_skeleton.py
@@ -12,47 +12,39 @@ def pwm_percent(percent):
 
 
 # Define pins to use with DRV8833
-AIN1 = Pin(X, mode=Pin.OUT)
-AIN2 = Pin(XX, mode=Pin.OUT)
+AIN1 = Pin(32, mode=Pin.OUT)
+AIN2 = Pin(15, mode=Pin.OUT)
 
 #need to initalize BIN1 and BIN2 if using second motor
 #BIN1 = Pin(XX, mode=Pin.OUT)
 #BIN2 = Pin(XX, mode=Pin.OUT)
 
 # Forward at 100% speed
-pwm_MotorA1 = PWM(AIN1, freq=10, duty=pwm_percent(XX))
-pwm_MotorA2 = PWM(AIN2, freq=10, duty=pwm_percent(XX))
+pwm_MotorA1 = PWM(AIN1, freq=10, duty=pwm_percent(100))
+pwm_MotorA2 = PWM(AIN2, freq=10, duty=pwm_percent(100))
 #pwm_MotorB1 = PWM(BIN1, freq=10, duty=pwm_percent())
 #pwm_MotorB2 = PWM(BIN2, freq=10, duty=pwm_percent())
 sleep(2)
 
 
 # Forward at 50% speed
-pwm_MotorA1.duty(pwm_percent(XX))
-pwm_MotorA2.duty(pwm_percent(XX))
-#pwm_MotorB1.duty(pwm_percent(XX))
-#pwm_MotorB2.duty(pwm_percent(XX))
+pwm_MotorA1.duty(pwm_percent(50))
+pwm_MotorA2.duty(pwm_percent(0))
 sleep(2)
 
 # Stop
-pwm_MotorA1.duty(pwm_percent(100))
-pwm_MotorA2.duty(pwm_percent(100))
-# pwm_MotorB1.duty((100))
-# pwm_MotorB2.duty((100))
+pwm_MotorA1.duty(pwm_percent(0))
+pwm_MotorA2.duty(pwm_percent(0))
 sleep(2)
 
 # Reverse at 50% speed
-pwm_MotorA1.duty(pwm_percent(XX))
-pwm_MotorA2.duty(pwm_percent(XX))
-# pwm_MotorB1.duty(pwm_percent(X))
-# pwm_MotorB2.duty(pwm_percent(XX))
+pwm_MotorA1.duty(pwm_percent(0))
+pwm_MotorA2.duty(pwm_percent(50))
 sleep(2)
 
 # Reverse at 100% speed
-pwm_MotorA1.duty(pwm_percent(XX))
-pwm_MotorA2.duty(pwm_percent(XX))
-# pwm_MotorB1.duty(pwm_percent(X))
-# pwm_MotorB2.duty(pwm_percent(XX))
+pwm_MotorA1.duty(pwm_percent(0))
+pwm_MotorA2.duty(pwm_percent(100))
 sleep(2)
 
 
@@ -62,6 +54,7 @@ pwm_MotorA1.deinit()
 pwm_MotorA2.deinit()
 # pwm_MotorB1.deinit()
 # pwm_MotorB2.deinit()
+
 
 
 
